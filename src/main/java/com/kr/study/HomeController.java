df16321ec0e75aead447d733c74092cfd25bc77d
beemilly <beemillyjp@gmail.com>
2021-03-27 13:22:37 +0900

    0327

--- src/main/java/com/kr/study/HomeController.java
+++ src/main/java/com/kr/study/HomeController.java
@@ -30,7 +30,7 @@ public class HomeController {
 		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
 		
 		String formattedDate = dateFormat.format(date);
-		String test = "git test0320"; // git test 0327 
+		String test = "git test0327";
  		model.addAttribute("serverTime", formattedDate );
 		
 		return "home";
